d445c9597124619731a4bcafcddd3096ddd52b50
dylanbsgit <dbsoto@uncg.edu>
2025-05-31 19:55:24 -0400

    Init commit

---
+++ src/main/java/com/animalgallery/animal_gallery/AnimalGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.animalgallery.animal_gallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animalgallery/animal_gallery/AnimalGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animalgallery.animal_gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
