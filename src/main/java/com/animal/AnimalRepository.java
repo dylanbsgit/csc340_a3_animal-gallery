342a479bda680267c19a7a6f925f14af0af90d1a
dylanbsgit <dbsoto@uncg.edu>
2025-06-01 16:14:10 -0400

    AnimalRepo: Add methods

--- src/main/java/com/animal/AnimalRepository.java
+++ src/main/java/com/animal/AnimalRepository.java
@@ -1,5 +1,18 @@
 package com.animal;
 
-public class AnimalRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    // Find animals by name
+    List<Animal> findByName(String name);
+
+    // Custom query: Find animals whose favorite food matches
+    @Query("SELECT a FROM Animal a WHERE a.favoriteFood = ?1")
+    List<Animal> findByFavoriteFood(String favoriteFood);
 }
