21d04af899c6b20471c55bf2c0060df48d8bd49f
dylanbsgit <dbsoto@uncg.edu>
2025-06-02 08:28:40 -0400

    @Service

--- src/main/java/com/animal/AnimalService.java
+++ src/main/java/com/animal/AnimalService.java
@@ -1,5 +1,42 @@
 package com.animal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AnimalService {
-    
+
+    private final AnimalRepository animalRepository;
+
+    @Autowired
+    public AnimalService(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+    public Optional<Animal> getAnimalById(Long id) {
+        return animalRepository.findById(id);
+    }
+
+    public List<Animal> getAnimalsByName(String name) {
+        return animalRepository.findByName(name);
+    }
+
+    public List<Animal> getAnimalsByFavoriteFood(String favoriteFood) {
+        return animalRepository.findByFavoriteFood(favoriteFood);
+    }
+
+    public Animal saveAnimal(Animal animal) {
+        return animalRepository.save(animal);
+    }
+
+    public void deleteAnimal(Long id) {
+        animalRepository.deleteById(id);
+    }
 }
